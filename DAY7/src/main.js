47976cbb14323a6edb14a082442caecac3afe92f
Mohammad Shoaib <mohdshoaibm487@gmail.com>
2026-01-12 21:45:02 +0530

    useState

--- DAY7/src/main.js
+++ DAY7/src/main.js
@@ -8,7 +8,7 @@ function Main()
     const [count,setCount]=useState(0);
    return( <>
             <div className="counter">   
-                <h1>Counter:{count}</h1>
+                <h1>Counter Is:{count}</h1>
                 <button onClick={()=>setCount(count+1)}>Increment</button>
                 <button onClick={()=>setCount(count-1)}>Decrement</button>
             </div>
