ffb4e73b74e9d70d4d6c2b630dbff7b8365079cd
Mohammad Shoaib <mohdshoaibm487@gmail.com>
2026-01-18 23:06:41 +0530

    redux

---
+++ DAY14/src/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+    return(
+        <>
+            <h1>Redux</h1>
+        </>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
